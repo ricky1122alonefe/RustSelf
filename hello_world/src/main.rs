d10e1e80e93066fe8acc490c0d66b6f1e85a4bb8
张睿 <zr@xingwei.com>
2020-12-30 14:44:55 +0800

    更新：init

---
+++ hello_world/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+  println!("{}",gcd(10,10));
+}
+
+fn gcd(mut n:u64,mut m :u64) -> u64{
+    assert!(n!=0 && m!=0);
+    while m !=0{
+        if m< n {
+            let t = m ;
+            m = n ;
+            n = t;
+        }
+        m = m %n ;
+    }
+    n
+}
